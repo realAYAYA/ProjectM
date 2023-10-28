26e154f026c5e68348fff5c2987d40a9685c12a0
Liudong <l1206776367@yahoo.com>
2023-10-28 21:18:50 +0800

    none

--- Demo/Source/DemoEditor.Target.cs
+++ Demo/Source/DemoEditor.Target.cs
@@ -9,7 +9,7 @@ public class DemoEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("Demo");
 	}
 }
